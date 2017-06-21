853fe51789974feb5932266210b15d0dc4660938
Vjsubramanian <sarathy.fx@gmail.com>
2017-06-21 13:54:44 -0500

    first commit

---
+++ scripts/landing.js
@@ -0,0 +1,25 @@
+v var pointsArray = document.getElementsByClassName('point');
+
+ var revealPoint = function(point) {
+         point.style.opacity = 1;
+         point.style.transform = "scaleX(1) translateY(0)";
+         point.style.msTransform = "scaleX(1) translateY(0)";
+         point.style.WebkitTransform = "scaleX(1) translateY(0)";
+        };
+ 
+ var animatePoints = function(points) {
+      forEach(points, revealPoint);
+    
+ };
+
+ window.onload = function() {
+    if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+     window.addEventListener('scroll', function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+     }
